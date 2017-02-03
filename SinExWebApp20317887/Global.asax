53990b9b212e54a359e14796795ccfbe810baf60
tksoab <tksoab@CSL4WK50>
2017-02-03 16:47:16 +0800

    Add project files.

---
+++ SinExWebApp20317887/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SinExWebApp20317887.MvcApplication" Language="C#" %>
---
+++ SinExWebApp20317887/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SinExWebApp20317887.Startup))]
+namespace SinExWebApp20317887
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
